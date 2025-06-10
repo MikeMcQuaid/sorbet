498857173eaab61f8a7241c1bc3981befb0c313a
Jake Zimmerman <zimmerman.jake@gmail.com>
2025-06-10 14:06:11 -0700

    prework: fastmoda isIncompatibleOverride allowIncompatibleOverrideAll

--- definition_validator/validator.cc
+++ definition_validator/validator.cc
@@ -519,7 +519,7 @@ void validateOverriding(const core::Context ctx, const ast::ExpressionPtr &tree,
     }
 
     if (overriddenMethods.size() == 0 && method.data(ctx)->flags.isOverride &&
-        !method.data(ctx)->flags.isIncompatibleOverride) {
+        !method.data(ctx)->flags.allowIncompatibleOverrideAll) {
         if (auto e = ctx.state.beginError(method.data(ctx)->loc(), core::errors::Resolver::BadMethodOverride)) {
             e.setHeader("Method `{}` is marked `{}` but does not override anything", method.show(ctx), "override");
         }
@@ -570,7 +570,7 @@ void validateOverriding(const core::Context ctx, const ast::ExpressionPtr &tree,
         }
         if ((overriddenMethod.data(ctx)->flags.isAbstract || overriddenMethod.data(ctx)->flags.isOverridable ||
              (overriddenMethod.data(ctx)->hasSig() && method.data(ctx)->flags.isOverride)) &&
-            !method.data(ctx)->flags.isIncompatibleOverride && !isRBI &&
+            !method.data(ctx)->flags.allowIncompatibleOverrideAll && !isRBI &&
             !method.data(ctx)->flags.isRewriterSynthesized &&
             overriddenMethod != core::Symbols::BasicObject_initialize()) {
             // We only ignore BasicObject#initialize for backwards compatibility.
